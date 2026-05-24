2a491b876c09ac90ef89e123a24ab471d4c79501
Banusriprakash <157564126+banusriprakash@users.noreply.github.com>
2026-05-24 16:59:02 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        mp={}
+        for st in strs:
+            org=st
+            cp="".join(sorted(st))
+            if cp in mp:
+                mp[cp].append(org)
+
+            else:
+                ls=[]
+                ls.append(org)
+                mp[cp]=ls
+
+        return list(mp.values())
+
