efe68854a2b14e2e0c2b826b78d51900b3bfa231
Banusriprakash <157564126+banusriprakash@users.noreply.github.com>
2026-07-16 16:33:12 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        if not s:
+            return 0
+
+        p1=0
+
+        for p2 in range(len(t)):
+            if s[p1]==t[p2]:
+                p1+=1
+
+        return len(t)-p1
+       
+        
