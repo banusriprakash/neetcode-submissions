933f4d5ad60fd4c4f498e7dec55cdcb785565cfb
Banusriprakash <157564126+banusriprakash@users.noreply.github.com>
2026-07-28 16:35:12 +0530

    Add: intersection-of-two-arrays - submission-7

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if not nums1 or not nums2:
+            return []
+
+        ans=[]
+        
+        mp={}
+
+        for val1 in nums1:
+            mp[val1]=mp.get(val1,0)+1
+
+        for val2 in nums2:
+            if val2 in mp:
+                if val2 not in ans:
+                    ans.append(val2)
+
+            
+
+
+
+        return ans
