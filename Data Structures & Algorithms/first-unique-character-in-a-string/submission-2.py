df23c5fe7fddf5e9115314296d5c859311af886b
Banusriprakash <157564126+banusriprakash@users.noreply.github.com>
2026-07-30 18:02:03 +0530

    Add: first-unique-character-in-a-string - submission-2

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+
+        mp=defaultdict(list)
+        for i in range(len(s)):
+            mp[s[i]].append(i)
+
+        sorted_dict=dict(sorted(mp.items(),key=lambda val:len(val[1])))
+        print(sorted_dict)
+
+        
+        ans=-1
+        for val in sorted_dict.values():
+            if len(val)==1:
+                ans=val[0]
+                return ans
+
+        return ans
